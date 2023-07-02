5204fc6946c90522f85dfc4a7c2618327e4c1e7c
Netson Cavina <netsoncavina@gmail.com>
2023-07-02 14:01:14 -0300

    Adição do navigation

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../../routes";
 
 interface Props {
   animeId: string;
@@ -7,41 +10,54 @@ interface Props {
   animeImg: string;
   releaseDate: string;
 }
+export type StackNavigation = StackNavigationProp<RootStackParamList>;
 
 const AnimeCard = (props: Props) => {
+  const navigation = useNavigation<StackNavigation>();
   return (
-    <View
-      style={{
-        borderRadius: 10,
-        // marginRight: 15,
-      }}
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("AnimeScreen", {
+          animeTitle: props.animeTitle,
+          animeId: props.animeId,
+          animeImg: props.animeImg,
+          releaseDate: props.releaseDate,
+        } as any)
+      }
     >
-      <Image
-        source={{ uri: props.animeImg }}
-        style={{ width: 200, height: 300, borderRadius: 10 }}
-      />
       <View
         style={{
-          padding: 10,
-          backgroundColor: "rgba(0,0,0,0.8)",
-          position: "absolute",
-          bottom: 0,
-          width: 200,
-          borderBottomLeftRadius: 10,
-          borderBottomRightRadius: 10,
+          borderRadius: 10,
+          // marginRight: 15,
         }}
       >
-        <Text
-          style={{ fontSize: 24, fontWeight: "bold", color: "#d31a48" }}
-          numberOfLines={2}
+        <Image
+          source={{ uri: props.animeImg }}
+          style={{ width: 200, height: 300, borderRadius: 10 }}
+        />
+        <View
+          style={{
+            padding: 10,
+            backgroundColor: "rgba(0,0,0,0.8)",
+            position: "absolute",
+            bottom: 0,
+            width: 200,
+            borderBottomLeftRadius: 10,
+            borderBottomRightRadius: 10,
+          }}
         >
-          {props.animeTitle}
-        </Text>
-        <Text style={{ fontSize: 15, fontWeight: "bold", color: "gray" }}>
-          {props.releaseDate}
-        </Text>
+          <Text
+            style={{ fontSize: 24, fontWeight: "bold", color: "#d31a48" }}
+            numberOfLines={2}
+          >
+            {props.animeTitle}
+          </Text>
+          <Text style={{ fontSize: 15, fontWeight: "bold", color: "gray" }}>
+            {props.releaseDate}
+          </Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
