8bfba3fa8e14c36312ff5f9f8b64db9b8e57e8b0
Netson Cavina <netsoncavina@gmail.com>
2023-07-03 16:37:39 -0300

    Adição dos estados

--- App.tsx
+++ App.tsx
@@ -5,14 +5,27 @@ import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
 import Topbar from "./components/Topbar/Topbar";
 import Routes from "./routes";
-
+import SearchResult from "./components/SearchResult/SearchResult";
 export default function App() {
   const [topBarVisible, setTopBarVisible] = useState(true);
+  const [searchData, setSearchData] = useState([]);
+  const [queryLength, setQueryLength] = useState(0);
+
   return (
     <PaperProvider>
       <StatusBar style="auto" />
-      <Topbar showTopbar={topBarVisible} />
+      <Topbar
+        showTopbar={topBarVisible}
+        setSearchData={setSearchData}
+        setQueryLength={setQueryLength}
+      />
       <NavigationContainer>
+        <SearchResult
+          data={searchData}
+          isVisible={true}
+          queryLength={queryLength}
+          setQueryLength={setQueryLength}
+        />
         <Routes />
       </NavigationContainer>
     </PaperProvider>
