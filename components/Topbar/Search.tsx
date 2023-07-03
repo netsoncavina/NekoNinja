d9cfccebaf4b01f4859814b664b2cffd38b769e0
Netson Cavina <netsoncavina@gmail.com>
2023-07-03 16:37:09 -0300

    Adição de query para pesquisa

--- components/Topbar/Search.tsx
+++ components/Topbar/Search.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { searchAnime } from "../../api/api";
 
-const Search = () => {
-  const [searchQuery, setSearchQuery] = React.useState("");
+interface Props {
+  setSearchData: any;
+  setQueryLength: any;
+}
 
-  const onChangeSearch = (query: any) => setSearchQuery(query);
+const Search = (props: Props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query: string) => {
+    setSearchQuery(query);
+    searchAnime(query).then((res) => {
+      // console.log(res.data);
+      props.setSearchData(res.data);
+    });
+    props.setQueryLength(query.length);
+  };
 
   return (
     <Searchbar
