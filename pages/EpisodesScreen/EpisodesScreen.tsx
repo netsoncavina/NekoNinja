235f3e3269f0d43d7ba227002cb0d7ddf3c8170f
Netson Cavina <netsoncavina@gmail.com>
2023-07-06 17:23:31 -0300

    Desenvolvimento pagina de episodios

---
+++ pages/EpisodesScreen/EpisodesScreen.tsx
@@ -0,0 +1,73 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import EpisodeButton from "./EpisodeButton";
+
+interface Props {
+  animeTitle: string;
+  episodes: Array<any>;
+  color: string;
+}
+
+const EpisodesScreen = ({ route }: any) => {
+  const { animeTitle, episodes, color } = route.params;
+  //   console.log(episodes);
+
+  return (
+    <ScrollView>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 30,
+          fontWeight: "bold",
+          textAlign: "center",
+          padding: 10,
+          backgroundColor: "#212427",
+          borderRadius: 10,
+          margin: 10,
+        }}
+      >
+        {animeTitle}
+      </Text>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 20,
+          fontWeight: "bold",
+          textAlign: "center",
+          padding: 10,
+          backgroundColor: "#212427",
+          borderRadius: 10,
+          margin: 10,
+        }}
+      >
+        Lista de episódios
+      </Text>
+      <View
+        style={{
+          backgroundColor: "#212427",
+          padding: 5,
+
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {episodes.map((episode: any) => (
+          <EpisodeButton
+            key={episode.id}
+            episodeNumber={episode.number}
+            episodeTitle={episode.title}
+            episodeId={episode.id}
+            color={color}
+          />
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default EpisodesScreen;
+
+const styles = StyleSheet.create({});
